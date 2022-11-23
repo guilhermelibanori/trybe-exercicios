b6d4869e11a15275f8bf5d048a2f431ea71f97df
unknown <guilhermelibanori1@gmail.com>
2022-11-23 19:01:43 -0300

    Exericicio 1&2

---
+++ modulo_javascript/exercicio.js
@@ -0,0 +1,25 @@
+// const a = 5
+// const b = 9
+
+// console.log(a + b)
+
+const valorA = 9
+const valorB = 19
+const valorC = 11
+
+// if(valorA > valorB) {
+//     console.log("Valor A é maior") 
+// }
+// else{
+//     console.log("Valor B é maior") 
+// }
+
+if(valorA > valorB && valorA > valorC){
+    console.log("O valor A é o maior")
+}
+else if(valorB > valorA && valorB > valorC){
+    console.log("O valor B é o maior")
+}
+else{
+    console.log("O valor C é o maior")
+}
