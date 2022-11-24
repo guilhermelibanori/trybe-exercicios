72132905a0e2bf2f578f7dcc0ec0e5b3a384df93
unknown <guilhermelibanori1@gmail.com>
2022-11-23 22:55:44 -0300

    Descomentando

--- modulo_javascript/exercicio.js
+++ modulo_javascript/exercicio.js
@@ -1,138 +1,138 @@
-// const a = 5
-// const b = 9
+const a = 5
+const b = 9
 
-// console.log(a + b)
+console.log(a + b)
 
-// const valorA = 9
-// const valorB = 19
-// const valorC = 11
+const valorA = 9
+const valorB = 19
+const valorC = 11
 
-// if(valorA > valorB) {
-//     console.log("Valor A é maior") 
-// }
-// else{
-//     console.log("Valor B é maior") 
-// }
+if(valorA > valorB) {
+    console.log("Valor A é maior") 
+}
+else{
+    console.log("Valor B é maior") 
+}
 
-// if(valorA > valorB && valorA > valorC){
-//     console.log("O valor A é o maior")
-// }
-// else if(valorB > valorA && valorB > valorC){
-//     console.log("O valor B é o maior")
-// }
-// else{
-//     console.log("O valor C é o maior")
-// }
+if(valorA > valorB && valorA > valorC){
+    console.log("O valor A é o maior")
+}
+else if(valorB > valorA && valorB > valorC){
+    console.log("O valor B é o maior")
+}
+else{
+    console.log("O valor C é o maior")
+}
 
-// let valor = 1
+let valor = 1
 
-// if(valor > 0){
-//     console.log("Positive")
-// }
-// else if(valor < 0){
-//     console.log("Negative")
-// }
-// else{
-//     console.log("Zero")
-// }
+if(valor > 0){
+    console.log("Positive")
+}
+else if(valor < 0){
+    console.log("Negative")
+}
+else{
+    console.log("Zero")
+}
 
-// let angulo1 = 10;
-// let angulo2 = 90;
-// let angulo3 = 42;
-// let somaAngulo = angulo1 + angulo2 + angulo3;
+let angulo1 = 10;
+let angulo2 = 90;
+let angulo3 = 42;
+let somaAngulo = angulo1 + angulo2 + angulo3;
 
-// if(somaAngulo === 180){
-//     console.log('true');
-// }
-// else if(somaAngulo < 180){
-//     console.log('False');
-// }
-// else if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0 || !angulo1 || !angulo2 || !angulo3){
-//     console.log('Ângulo Invalido');
-// }
+if(somaAngulo === 180){
+    console.log('true');
+}
+else if(somaAngulo < 180){
+    console.log('False');
+}
+else if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0 || !angulo1 || !angulo2 || !angulo3){
+    console.log('Ângulo Invalido');
+}
 
-// let chess = '?';
-// let chesslowercase = chess.toLowerCase();
-// let movement = '?';
-//     switch(chesslowercase){
-//         case 'bishop':
-//             movement = 'Moves diagonally';
-//             break;
-//         case 'pawn':
-//             movement = 'One Square Straight Ahead';
-//             break;
-//         case 'rook':
-//             movement = 'Its line or row';
-//             break;
-//         case 'knight':
-//             movement = 'Jumps in making the shortest move that is not a straight one';
-//             break;
-//         case 'queen':
-//             movement = 'May move like a Rook or a Bishop';
-//             break;
-//         case 'king':
-//             movement = 'Moves from its square to a neighboring square';
-//             break;
-//         default: 
-//             movement = 'Not a valid piece';
-//           }
+let chess = '?';
+let chesslowercase = chess.toLowerCase();
+let movement = '?';
+    switch(chesslowercase){
+        case 'bishop':
+            movement = 'Moves diagonally';
+            break;
+        case 'pawn':
+            movement = 'One Square Straight Ahead';
+            break;
+        case 'rook':
+            movement = 'Its line or row';
+            break;
+        case 'knight':
+            movement = 'Jumps in making the shortest move that is not a straight one';
+            break;
+        case 'queen':
+            movement = 'May move like a Rook or a Bishop';
+            break;
+        case 'king':
+            movement = 'Moves from its square to a neighboring square';
+            break;
+        default: 
+            movement = 'Not a valid piece';
+          }
 
-//         console.log(movement)
+        console.log(movement)
 
-// let porcentagem = 11;
-// let nota = '';
-//     if(porcentagem >= 90){
-//         nota = 'A';
-//     }
-//     else if(porcentagem >= 80 && porcentagem < 90){
-//         nota = 'B';
-//     }
-//     else if(porcentagem >= 70 && porcentagem < 80){
-//         nota = 'C';
-//     }
-//     else if(porcentagem >= 60 && porcentagem < 70){
-//         nota = 'D';
-//     }
-//     else if(porcentagem >= 50 && porcentagem < 60){
-//         nota = 'E';
-//     }
-//     else if(porcentagem <= 50 && porcentagem >= 0){
-//         nota = 'F';
-//     }
-//     else if(porcentagem >= 100 || porcentagem < 0){
-//         nota = 'Maior que 100 ou menor que 100 nota ínvalida';
-//     }
-//     console.log(nota)
+let porcentagem = 11;
+let nota = '';
+    if(porcentagem >= 90){
+        nota = 'A';
+    }
+    else if(porcentagem >= 80 && porcentagem < 90){
+        nota = 'B';
+    }
+    else if(porcentagem >= 70 && porcentagem < 80){
+        nota = 'C';
+    }
+    else if(porcentagem >= 60 && porcentagem < 70){
+        nota = 'D';
+    }
+    else if(porcentagem >= 50 && porcentagem < 60){
+        nota = 'E';
+    }
+    else if(porcentagem <= 50 && porcentagem >= 0){
+        nota = 'F';
+    }
+    else if(porcentagem >= 100 || porcentagem < 0){
+        nota = 'Maior que 100 ou menor que 100 nota ínvalida';
+    }
+    console.log(nota)
 
-// const numeroA = 19;
-// const numeroB = 42;
-// const numeroC = 12;
-//     if(numeroA % 2 === 0|| numeroB % 2 === 0 || numeroC % 2 === 0){
-//         console.log('True')
-//     }
-//     else{
-//         console.log('False')
-//     }
+const numeroA = 19;
+const numeroB = 42;
+const numeroC = 12;
+    if(numeroA % 2 === 0|| numeroB % 2 === 0 || numeroC % 2 === 0){
+        console.log('True')
+    }
+    else{
+        console.log('False')
+    }
     
-//     if(numeroA % 2 === 1|| numeroB % 2 === 1 || numeroC % 2 === 1){
-//         console.log('True')
-//     }
-//     else{
-//         console.log('False')
-//     }
+    if(numeroA % 2 === 1|| numeroB % 2 === 1 || numeroC % 2 === 1){
+        console.log('True')
+    }
+    else{
+        console.log('False')
+    }
 
-// const valorDeCompra = 14;
-// const valorDeVenda = 19;
-// const imposto = 0.2;
-// let custoDoProduto = 14 * imposto;
-// let lucro
-// if(valorDeCompra <= 0 || valorDeVenda <= 0){
-//     console.log('Você precisa colocar um valor valido')
-// }
-// else{
-//     lucro = valorDeVenda - custoDoProduto
-//     console.log(lucro)
-// }
+const valorDeCompra = 14;
+const valorDeVenda = 19;
+const imposto = 0.2;
+let custoDoProduto = 14 * imposto;
+let lucro
+if(valorDeCompra <= 0 || valorDeVenda <= 0){
+    console.log('Você precisa colocar um valor valido')
+}
+else{
+    lucro = valorDeVenda - custoDoProduto
+    console.log(lucro)
+}
 
 let desconto = '?';
 let salarioBruto = 3000;
