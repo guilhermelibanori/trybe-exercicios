443b41430fb0d0cec7c08eb7b646e3f0d87f295a
unknown <guilhermelibanori1@gmail.com>
2022-11-26 19:51:18 -0300

    Reorganizando tudo

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia02-javascript-Array_e_loop_For/aulas.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Abacaxi')
+let menuServices = menu.indexOf('Serviços')
+
+console.log(menu);
+
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index+= 1){
+    console.log(groceryList[index])
+}
--- modulo_javascript/dia2-arrayloopfor/arrays.js
+++
@@ -1,5 +0,0 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Abacaxi')
-let menuServices = menu.indexOf('Serviços')
-
-console.log(menu);
