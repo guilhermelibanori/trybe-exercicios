9af471a793f986118a3615d4146f8d18f254ad3f
unknown <guilhermelibanori1@gmail.com>
2022-11-29 18:58:45 -0300

    Segundo exercicio

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia03-javascript-logica_de_programacao_e_algoritmos/dia03-javascript-logica-de-programacao.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia03-javascript-logica_de_programacao_e_algoritmos/dia03-javascript-logica-de-programacao.js
@@ -1,9 +1,20 @@
 // O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
 // 4! = 4 x 3 x 2 x 1 = 24
 // Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
-const number = 10;
-let factorial = 1;
-for(let index = 1; index <= number; index += 1){
-    factorial *= index;
+// const number = 10;
+// let factorial = 1;
+// for(let index = 1; index <= number; index += 1){
+//     factorial *= index;
+// }
+// console.log(factorial)
+
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let wordBackwards = '';
+
+for(index = word.length; index >= 0; index -= 1){
+    wordBackwards += word.charAt(index)
+    console.log(wordBackwards)
 }
-console.log(factorial)
