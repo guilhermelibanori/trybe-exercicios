6f1aae324943d80330b381de02c965a293883aaf
unknown <guilhermelibanori1@gmail.com>
2022-11-30 15:48:23 -0300

    Primeiras impressões

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/aulas.js
@@ -0,0 +1,41 @@
+// let diasDaSemana = {
+//     1: 'domingo',
+//     2: 'segunda',
+//     3: 'terça',
+//     4: 'quarta',
+//     5: 'quinta',
+//     6: 'sexta',
+//     7: 'sábado',
+//   };
+  
+//   diasDaSemana.1; // SyntaxError: Unexpected number
+//   console.log(diasDaSemana['1']); // domingo
+
+// let conta = {
+//     agencia: '0000',
+//     banco: {
+//       cod: '012',
+//       id: 4,
+//       nome: 'TrybeBank',
+//     },
+//   };
+  
+//   let infoDoBanco = 'banco';
+//   console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+//   console.log(conta[infoDoBanco]['nome']); // TrybeBank
+  
+//   console.log(conta.agencia); // 0000
+//   console.log(conta['agencia']); // 0000
+  
+//   console.log(conta.banco.cod); // 012
+//   console.log(conta['banco']['id']); // 4
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, golden: 3}
+};
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+console.log("A jogadora " + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+console.log(player)
