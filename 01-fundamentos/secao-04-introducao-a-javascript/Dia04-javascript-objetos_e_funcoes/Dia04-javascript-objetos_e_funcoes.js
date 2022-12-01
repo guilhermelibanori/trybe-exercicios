0653019433df75cb260bf356ddca93d4237a9adb
unknown <guilhermelibanori1@gmail.com>
2022-11-30 22:07:00 -0300

    exercicios objeto

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-objetos_e_funcoes.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-objetos_e_funcoes.js
@@ -11,19 +11,19 @@ let info = {
     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
     nota: 'O último MacPatinhas',
   };
-info.recorrente = "Sim"
-infoo.recorrente = "Sim"
+info.recorrente = "Sim";
+infoo.recorrente = "Sim";
 //   Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.
 //    Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
 //   Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 //    Use a sintaxe meuObjeto.chave. Valor esperado no console:
 
-// console.log(info.recorrente)
+console.log(info.recorrente)
 
 // Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
-let infoOne = []
-let infoTwo = []
-let together = []
+let infoOne = [];
+let infoTwo = [];
+let together = [];
 for (let index in info){
     infoOne.push(info[index])
 }
@@ -41,3 +41,29 @@ for (index = 0; index < infoOne.length; index += 1){
 }
 
 console.log(together)
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+//   Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte 
+//   formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+console.log("O livro favorito de " + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo + ' ')
+
+// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+let obj = {titulo: 'Harry Potter e o Prisioneiro de Azkaban', autor: 'JK Rowling', editora: 'Rocco'}
+leitor.livrosFavoritos.push(obj)
+let quantity = Math.floor(Math.random() * 1000)
+
+
+console.log(leitor.nome + ' tem ' + quantity + ' livros favoritos')
