270b4c36a1334299a80e7118a68c89f24694b5df
unknown <guilhermelibanori1@gmail.com>
2022-11-30 22:46:33 -0300

    Preso na funcao de repetir

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
@@ -1,12 +1,8 @@
 // Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
-
 // Exemplo de palíndromo: arara
-
 // verificaPalindrome('arara')
-
 // Retorno esperado: true
 // verificaPalindrome('desenvolvimento')
-
 // Retorno esperado: false
 
 // let wordBackwards = ''
@@ -24,6 +20,10 @@
 // }
 
 // console.log(palindromo('arara'))
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+
 // let array = [2, 3, 6, 7, 10, 1];
 // let highestNumber = array[0];
 
@@ -38,17 +38,58 @@
 // }
 // console.log(higherNumber(array))
 
-let array = [2, 4, 6, 7, 10, 0, -3];
-let lowestNumber = array[0];
+// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
-function lowerNumber(number){
-    for (index = 0; index < number.length; index += 1){
-        checkNumber = number[index]
-        if(checkNumber < lowestNumber){
-            lowestNumber = index
+// let array = [2, 4, 6, 7, 10, 0, -3];
+// let lowestNumber = array[0];
+
+// function lowerNumber(number){
+//     for (index = 0; index < number.length; index += 1){
+//         checkNumber = number[index];
+//         if(checkNumber < lowestNumber){
+//             lowestNumber = index;
+//         }
+//     }
+//     return lowestNumber
+// }
+
+// console.log(lowerNumber(array))
+
+
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+// let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// let biggestName = array[0];
+
+// function biggest(param){
+//     for (index = 0; index < param.length; index += 1){
+//         checkWord = param[index];
+//         if(checkWord.length > biggestName.length){
+//             biggestName = checkWord;
+//         }
+//     }
+//     return biggestName
+// }
+
+// console.log(biggest(array))
+
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+let array = [2, 3, 2, 5, 8, 2, 3];
+let repeats = 0;
+
+function repeat(param){
+    for (let index = 0; index < param.length; index += 1) {
+        checkNumber = param[index];
+        for (let i = 0; i < param.length; i += 1){
+            checkAgain = param[i]
+            if(checkNumber === checkAgain){
+                repeats += checkAgain
+                break;
+            }
         }
     }
-    return lowestNumber
+    return repeats
 }
 
-console.log(lowerNumber(array))
+console.log(repeat(array))
