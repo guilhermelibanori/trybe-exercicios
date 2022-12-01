fa1ed101f2e1659408088babae85131c1b687298
unknown <guilhermelibanori1@gmail.com>
2022-11-30 22:34:37 -0300

    3 primeiros exercicios

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
@@ -9,18 +9,46 @@
 
 // Retorno esperado: false
 
-let wordBackwards = ''
+// let wordBackwards = ''
 
-function palindromo(param){
-    for (let index = param.length; index >= 0; index -= 1){
-        wordBackwards += param.charAt(index)
+// function palindromo(param){
+//     for (let index = param.length; index >= 0; index -= 1){
+//         wordBackwards += param.charAt(index)
+//     }
+//     if (wordBackwards === param){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(palindromo('arara'))
+// let array = [2, 3, 6, 7, 10, 1];
+// let highestNumber = array[0];
+
+// function higherNumber(number){
+//     for (index = 0; index < number.length; index += 1){
+//         checkNumber = number[index];
+//         if(checkNumber > highestNumber){
+//             highestNumber = index;
+//         }
+//     }
+//     return highestNumber
+// }
+// console.log(higherNumber(array))
+
+let array = [2, 4, 6, 7, 10, 0, -3];
+let lowestNumber = array[0];
+
+function lowerNumber(number){
+    for (index = 0; index < number.length; index += 1){
+        checkNumber = number[index]
+        if(checkNumber < lowestNumber){
+            lowestNumber = index
+        }
     }
-    if (wordBackwards === param){
-        return true
-    }
-    else{
-        return false
-    }
+    return lowestNumber
 }
 
-console.log(palindromo('arara'))
+console.log(lowerNumber(array))
