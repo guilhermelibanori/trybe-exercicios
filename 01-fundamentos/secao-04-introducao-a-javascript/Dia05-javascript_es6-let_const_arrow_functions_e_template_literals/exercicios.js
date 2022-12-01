7f99275c6746d4d273d4eb0ce71efbc9362d0117
unknown <guilhermelibanori1@gmail.com>
2022-12-01 15:09:32 -0300

    Primeiros exercicios

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia05-javascript_es6-let_const_arrow_functions_e_template_literals/exercicios.js
@@ -0,0 +1,25 @@
+// let fora = 30;
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       fora += 1
+//       console.log('Idade dentro do for:', idade)
+//     }
+//     return console.log('Idade fora do for:', fora) // retire essa linha ao implementar o tipo de variável correta.
+//   }
+//   imprimeIdade()
+
+
+  // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa.nome = 'Luna'
+//   pessoa.idade = 19
+//  // Altere essa estrutura para corrigir o erro.
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
