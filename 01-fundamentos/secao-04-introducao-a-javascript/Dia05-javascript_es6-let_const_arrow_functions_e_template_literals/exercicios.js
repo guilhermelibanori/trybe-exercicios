f8b1fee58e9b0a66d2448e3ee95d4123e92a7d03
unknown <guilhermelibanori1@gmail.com>
2022-12-01 15:35:01 -0300

    Arrow functions

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia05-javascript_es6-let_const_arrow_functions_e_template_literals/exercicios.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia05-javascript_es6-let_const_arrow_functions_e_template_literals/exercicios.js
@@ -20,6 +20,22 @@
 //   console.log('Nome:', pessoa.nome);
 //   console.log('Idade:', pessoa.idade);
 
-let favoriteFood = 'Lasanha';
-favoriteFood = 'Hamburguer';
-console.log(favoriteFood);
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);
+
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log(`Olá, ${name} ${lastName}!`);
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de a + b é: ${soma(a,b)}`);
+
+//   console.log((() => Math.random())());
+let nome = 'Ivan';
+
+  console.log((nome => `Olá, ${nome}!`)(nome))
