a12b485938b0a77db0853dcd9db87fa003ef993a
unknown <guilhermelibanori1@gmail.com>
2022-11-30 22:58:41 -0300

    Finalizando e voltando pra o anterior

--- 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/Dia04-javascript-objetos_e_funcoes/Dia04-javascript-funcoes.js
@@ -75,21 +75,49 @@
 
 // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
-let array = [2, 3, 2, 5, 8, 2, 3];
-let repeats = 0;
+// let array = [2, 3, 2, 5, 8, 2, 3];
+// let repeats = 0;
 
-function repeat(param){
-    for (let index = 0; index < param.length; index += 1) {
-        checkNumber = param[index];
-        for (let i = 0; i < param.length; i += 1){
-            checkAgain = param[i]
-            if(checkNumber === checkAgain){
-                repeats += checkAgain
-                break;
-            }
-        }
+// function repeat(param){
+//     for (let index = 0; index < param.length; index += 1) {
+//         checkNumber = param[index];
+//         for (let i = 0; i < param.length; i += 1){
+//             checkAgain = param[i]
+//             if(checkNumber === checkAgain){
+//                 repeats += checkAgain
+//                 break;
+//             }
+//         }
+//     }
+//     return repeats
+// }
+
+// console.log(repeat(array))
+
+// Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+// let sum = 0;
+
+// function sumNaturals(n){
+//     for (index = 1; index <= n; index += 1){
+//         sum += index
+//     }
+//     return sum
+// }
+
+// console.log(sumNaturals(5))
+
+
+// Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word.
+//  Considere que a string ending sempre será menor que a string word.
+
+function checkEnding(word, ending){
+    if (word.endsWith(ending)){
+        return true
     }
-    return repeats
+    else{
+        return false
+    }
 }
 
-console.log(repeat(array))
+console.log(checkEnding('joaofernando', 'fernan'))
